8b2179e92dfa5db686d1e9b4320c0978d069f3c8
zumesan <zumehashi3240518@icloud.com>
2022-05-15 19:25:57 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController #application_controllerを継承
 
   def index
-    @posts = Post.all
-  end
+    @posts = Post.order(id:"DESC") #Postモデルのidをorderメソッドで降順にして、インスタンス変数@postsに代入
+  end #index.html.erbファイルへ飛ぶ
 
-  def new
-  end
+  #def new
+  #end
 
   def create
-    Post.create(content: params[:content])
+    Post.create(content: params[:content]) #保温
+    redirect_to action: :index #保存後にトップページへリダイレクトする。
   end
 end
